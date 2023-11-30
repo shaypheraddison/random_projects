59a90c7a1d269c6d319afc541f6a01da98d0677c
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-11-30 09:02:34 -0600

    Create simple_calc.py

---
+++ simple_calc.py
@@ -0,0 +1,5 @@
+#Going back to basics with a simple calculator
+
+first_number = int(input("First Number: ")
+second_number = int(input("Second Number: ")
+sum = print("Sum: "first_number + second_number)
